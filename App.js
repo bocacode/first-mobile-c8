f2f4f1a0a49ea22dbec8ccf227f13afafb1b090d
Todd Albert <todd@bocacode.com>
2022-11-08 15:17:20 -0500

    played with props, state, styling

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Counter from './components/Counter';
+import Welcome from './components/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome firstName={'Max'} />
+      <Welcome firstName={'Kevin'} />
+      <Text style={styles.bigText}>This is freaking cool! 😀</Text>
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  bigText: {
+    fontSize: '55vw',
+    color: 'white',
+    textAlign: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ea6',
     alignItems: 'center',
     justifyContent: 'center',
   },
